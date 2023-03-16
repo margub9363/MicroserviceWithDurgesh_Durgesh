2ceac7ab151cfc3e9b897ec2a1edbba56ea52ed1
rahman <margub.9363@gmail.com>
2023-03-16 08:47:26 +0530

    1:19 User Microservice is completed

--- UserService/src/main/java/com/lcwd/user/service/UserService/entities/User.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/entities/User.java
@@ -2,10 +2,9 @@ package com.lcwd.user.service.UserService.entities;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -24,5 +23,8 @@ public class User {
 
     @Column(name = "About")
     private String about;
+
+    @Transient
+    private List<Rating> ratings = new ArrayList<>();
 
 }
