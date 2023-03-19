59e32951c188f22089f6c1caf9976b6142cc8ae5
rahman <margub.9363@gmail.com>
2023-03-19 12:16:06 +0530

    3:31

--- UserService/src/main/java/com/lcwd/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,17 +1,22 @@
 package com.lcwd.user.service.UserService.services.impl;
 
 import com.lcwd.user.service.UserService.Exceptions.ResourceNotFoundException;
+import com.lcwd.user.service.UserService.entities.Hotel;
+import com.lcwd.user.service.UserService.entities.Rating;
 import com.lcwd.user.service.UserService.entities.User;
 import com.lcwd.user.service.UserService.repositories.UserRepository;
 import com.lcwd.user.service.UserService.services.UserServices;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -32,6 +37,7 @@ public class UserServiceImpl implements UserServices {
 
     @Override
     public List<User> getAllUser() {
+//
         return userRepository.findAll();
     }
 
@@ -41,9 +47,24 @@ public class UserServiceImpl implements UserServices {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given Id is not found on server " + userId));
 //        fetch rating for the above user from RATING-SERVICE
 //          http://localhost:8083/ratings/user/91457258-82c8-4a63-abcd-d1e135e82d33
-        ArrayList forObject = restTemplate.getForObject("http://localhost:8083/ratings/user/91457258-82c8-4a63-abcd-d1e135e82d33", ArrayList.class);
-        log.info("{}",forObject);
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/user/"+user.getUserId(), Rating[].class);
+        log.info("{}",ratingsOfUser);
 
+        List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+//            api call to hotel service to get the hotel
+//            http://localhost:8082/hotels/ce4206a1-4eef-4a7a-b76e-29eca1062114
+            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = forEntity.getBody();
+            log.info(hotel.toString());
+            log.info("response status code: {}",forEntity.getStatusCode());
+//            set the hotel to rating
+            rating.setHotel(hotel);
+//            return the rating
+            return rating;
+        }).collect(Collectors.toList());
+        user.setRatings(ratingList);
 
         return user;
     }
