69a267b5a245247fd0e522296c6d8e37016f62de
rahman <margub.9363@gmail.com>
2023-03-25 15:06:39 +0530

    Feing Client Interceptor

--- UserService/src/main/java/com/lcwd/user/service/UserService/config/Myconfig.java
+++ UserService/src/main/java/com/lcwd/user/service/UserService/config/Myconfig.java
@@ -3,6 +3,11 @@ package com.lcwd.user.service.UserService.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,5 +16,16 @@ public class Myconfig {
     @LoadBalanced
     public RestTemplate restTemplate (){
         return new RestTemplate();
+    }
+
+
+//    declare the bean of OAuth2AuthorizeClientManager
+    @Bean
+    public OAuth2AuthorizedClientManager manager(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository){
+
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,auth2AuthorizedClientRepository);
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+        return defaultOAuth2AuthorizedClientManager;
     }
 }
